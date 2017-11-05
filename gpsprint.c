15efef56cbebb666df2ed89bf614c0b0ebf3c831
MatthewShew <shewmj@gmail.com>
2017-11-05 01:40:32 -0700

    printing

--- gpsprint.c
+++ gpsprint.c
@@ -7,23 +7,44 @@ void printGpsData(struct gps_data_t * gpsdata) {
 	bool usedflags[MAXCHANNELS];
 	char scr[128];
 
-	//(void)unix_to_iso8601(gpsdata->fix.time, scr, sizeof(scr));
-	//fprintf(stdout, "%s\t%d\t%d\t\n\n", scr, gpsdata->fix.latitude, gpsdata->fix.longitude);
+	(void)unix_to_iso8601(gpsdata->fix.time, scr, sizeof(scr));
+	fprintf(stdout, "%s\t", scr, gpsdata->fix.latitude, gpsdata->fix.longitude);
+	if (gpsdata->fix.mode >= MODE_2D && isnan (gpsdata->fix.latitude) == 0)
+	{
+		fprintf (stdout, "Latitude: %s %c; ", deg_to_str(deg_type, fabs (gpsdata->fix.latitude)),
+		(gpsdata->fix.latitude < 0) ? 'S' : 'N');
 
-	for(int i = 0; i < MAXCHANNELS; i++) {
+		fprintf (stdout, "Latitude: %s %c; \n\n", deg_to_str(deg_type, fabs (gpsdata->fix.longitude)),
+		(gpsdata->fix.longitude < 0) ? 'E' : 'W');
 
-		usedflags[i] = false;
-		for(int j = 0; j < gpsdata->satellites_used; j++) {
-			if(gpsdata->skyview[i].used) {
-				usedflags[i] = true;
+		fflush (stdout);
+
+		for(int i = 0; i < MAXCHANNELS; i++) {
+
+			usedflags[i] = false;
+			for(int j = 0; j < gpsdata->satellites_used; j++) {
+				if(gpsdata->skyview[i].used) {
+					usedflags[i] = true;
+				}
 			}
-		}
-		if(gpsdata->satellites_visible != 0) {
-			for(int l = 0; l < MAX_POSSIBLE_SATS; l++) {
-				if(l < gpsdata->satellites_visible) {
-					//fprintf(stdout, "%3d\t%3d\t%3d\t%3d\t%c\n", gpsdata->skyview[l].PRN, gpsdata->skyview[l].elevation, gpsdata->skyview[l].azimuth, gpsdata->skyview[l].ss, usedflags[l] ? 'Y':'N');
+			if(gpsdata->satellites_visible != 0) {
+				for(int l = 0; l < MAX_POSSIBLE_SATS; l++) {
+					if(l < gpsdata->satellites_visible) {
+						if (gpsdata->fix.mode == MODE_2D) {
+							fprintf(stdout, "PRN: %3d Elevation: %02d Azimuth: %03d SNR: %02d Used: %c\n", gpsdata->skyview[l].PRN, gpsdata->skyview[l].elevation, gpsdata->skyview[l].azimuth, gpsdata->skyview[l].ss, usedflags[l] ? 'Y':'N');
+
+						} else {
+							fprintf(stdout, "PRN: %3d Elevation: N/A Azimuth: %03d SNR: %02d Used: %c\n", gpsdata->skyview[l].PRN, gpsdata->skyview[l].azimuth, gpsdata->skyview[l].ss, usedflags[l] ? 'Y':'N');
+
+						}
+					}
 				}
 			}
 		}
 	}
+	else
+		printf("n/a\n");
+
+
+	
 }
